123d092e7300c2ca7a1edf517760b6d761e2c021
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-29 11:21:36 -0600

    Initial Commit

---
+++ WhosThatDuckTwo/WhosThatDuckTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhosThatDuckTwoApp.swift
+//  WhosThatDuckTwo
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WhosThatDuckTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
